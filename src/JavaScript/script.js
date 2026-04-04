c65860d7db17478a876689eec0276a94c4842ae8
Lucas Kaoru <lucasnohara51@gmail.com>
2026-04-04 18:32:23 -0300

    refactor: melhora validação e tratamento de inputs no cadastro de vinhos

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -5,28 +5,53 @@ let quantidade;
 
 while (true) {
     nomeVinho = prompt("Digite o nome do vinho");
-    tipo = prompt("Digite o tipo do vinho");
-    safra = prompt("Digite o ano do vinho");
-    quantidade = prompt("Digite a quantidade em estoque do vinho");
 
-    if (nomeVinho === null || tipo === null || safra === null || quantidade === null) {
-        alert("Cadastro cancelado.");
-        break;
-    } else if (nomeVinho.trim() && tipo.trim() && Number.isInteger(Number(safra)) && Number(quantidade) > 0) {
-        alert("Cadastro realizado! Veja os detalhes no console.");
-        break;
+    if (nomeVinho === null || !nomeVinho.trim()) {
+        alert("O nome do vinho é obrigatorio");
     } else {
-        alert("Preencha todos os campos!");
-    }
-}
+        break;
+    };
+};
 
-if (nomeVinho && tipo && safra && quantidade) {
-    console.log(`
-====== DADOS DO VINHOS ======
+while (true) {
+    tipo = prompt("Digite o tipo do vinho");
+
+    if (tipo === null || !tipo.trim()) {
+        alert("O tipo do vinho é obrigatorio");
+    } else {
+        break;
+    };
+};
+
+while (true) {
+    safra = prompt("Digite o ano do vinho");
+    const ano = Number(safra);
+
+    if (safra === null || !Number.isInteger(ano) || ano <= 0) {
+        alert("A safra do vinho é obrigatorio");
+    } else {
+        break;
+    };
+};
+
+while (true) {
+    quantidade = prompt("Digite a quantidade em estoque do vinho");
+    const qtdd = Number(quantidade);
+
+    if (quantidade === null || !Number.isInteger(qtdd)) {
+        alert("A quantidade do vinho é obrigatorio");
+    } else if (qtdd <= 0) {
+        alert("A quantidade não pode ser menor ou igual a zero")
+    }else {
+        break;
+    };
+};
+
+console.log(`
+====== DADOS DOS VINHOS ======
     Nome do vinho: ${nomeVinho}
     Tipo do vinho: ${tipo}
     Sua safra: ${safra}
     Quantidade em estoque: ${quantidade}
 =============================
-    `);
-}
+`);
